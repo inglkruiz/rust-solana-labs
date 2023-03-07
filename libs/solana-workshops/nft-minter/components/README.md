a0d48731237b435794b3a04d21bf6fd23169cb96
Luis Ruiz <12603425+inglkruiz@users.noreply.github.com>
2023-03-07 23:39:00 +0100

    chore(solana-workshops): ntf-minter
    - copy/paste components

---
+++ libs/solana-workshops/nft-minter/components/README.md
@@ -0,0 +1,7 @@
+# solana-workshops-nft-minter-components
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test solana-workshops-nft-minter-components` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/solana-workshops/nft-minter/components/src/index.ts
@@ -0,0 +1,13 @@
+export { default as NetworkSwitcher } from './lib/NetworkSwitcher';
+export { default as NotificationList } from './lib/Notification';
+export { default as Text } from './lib/Text';
+export { default as NavElement } from './lib/nav-element';
+
+export * from './lib/AppBar';
+export * from './lib/ContentContainer';
+export * from './lib/Footer';
+export * from './lib/NftMinter';
+export * from './lib/RequestAirdrop';
+export * from './lib/SendTransaction';
+export * from './lib/SendVersionedTransaction';
+export * from './lib/SignMessage';
