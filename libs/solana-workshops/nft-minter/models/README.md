e8db185a0458aecd238edb943b23935f4bed2ca5
Luis Ruiz <12603425+inglkruiz@users.noreply.github.com>
2023-03-07 23:47:45 +0100

    chore(solana-workshops): ntf-minter
    - copy/paste models

---
+++ libs/solana-workshops/nft-minter/models/README.md
@@ -0,0 +1,7 @@
+# solana-workshops-nft-minter-models
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test solana-workshops-nft-minter-models` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/solana-workshops/nft-minter/models/jest.config.ts
@@ -0,0 +1,14 @@
+/* eslint-disable */
+export default {
+  displayName: 'solana-workshops-nft-minter-models',
+  preset: '../../../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]sx?$': [
+      '@swc/jest',
+      { jsc: { transform: { react: { runtime: 'automatic' } } } },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory:
+    '../../../../coverage/libs/solana-workshops/nft-minter/models',
+};
